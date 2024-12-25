a8b69fd2041abd4449acfca9bfdbdde856e8d644
Dystian En <distian.enyus@gmail.com>
2024-12-25 21:05:48 +0700

    hero section

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,6 +2,96 @@
 
 <?= $this->section('content') ?>
 
-<div class="tw-text-cream">Hello Bos</div>
+<section class="bg d-flex align-items-center" style="height: 670px">
+  <div class="row align-items-center" style="height: 100%">
+    <div class="gap-4 flex-column col-7 d-flex">
+      <h1 class="text-primary" style="font-size: 70px; font-weight: 700">FIND SHOES <br> THAT MATCHES <br> YOUR STYLE</h1>
+      <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+      <button class="px-5 rounded-pill btn btn-primary btn-lg" style="width: max-content;">Shop Now</button>
+
+      <div class="row">
+        <div class="col">
+          <h3 class="text-primary" style="font-weight: 700">200+</h3>
+          <p>International Brands</p>
+        </div>
+        <div class="col">
+          <h3 class="text-primary" style="font-weight: 700">300+</h3>
+          <p>High-Quality Products</p>
+        </div>
+        <div class="col">
+          <h3 class="text-primary" style="font-weight: 700">40,000+</h3>
+          <p>Happy Customers</p>
+        </div>
+      </div>
+    </div>
+    <div class="col" style="position: relative; height: 100%">
+      <img src="/imgs/people-style-2.png" alt="people-style-2">
+      <div style="position: absolute; bottom: 80px; right: 0px">
+        <img src="/imgs/people-style-1.png" alt="people-style-1">
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<section>
+  <div class="row">
+    <div class="col">
+      <img src="/imgs/logo_converse.svg" alt="">
+    </div>
+    <div class="col">
+      <img src="/imgs/logo_adidas.svg" alt="">
+    </div>
+    <div class="col">
+      <img src="/imgs/logo_reebok.svg" alt="">
+    </div>
+    <div class="col">
+      <img src="/imgs/logo_nb.svg" alt="">
+    </div>
+    <div class="col">
+      <img src="/imgs/logo_jordan.svg" alt="">
+    </div>
+    <div class="col">
+      <img src="/imgs/logo_asics.svg" alt="">
+    </div>
+  </div>
+</section>
+
+<section class="d-flex flex-column align-items-center" style="margin-top: 40px">
+  <h3 class="text-primary" style="font-weight: 700; font-size: 48px">NEW ARRIVALS</h3>
+
+  <div class="row">
+    <div class="col card-product">
+      <img src="/imgs/logo_converse.svg" alt="">
+      <h5 style="font-weight: 600;">Nike Air Max</h5>
+
+    </div>
+    <div class="col card-product">
+      <img src="/imgs/logo_adidas.svg" alt="">
+      <h5 style="font-weight: 600;">Nike Air Max</h5>
+
+    </div>
+    <div class="col card-product">
+      <img src="/imgs/logo_reebok.svg" alt="">
+      <h5 style="font-weight: 600;">Nike Air Max</h5>
+
+    </div>
+    <div class="col card-product">
+      <img src="/imgs/logo_nb.svg" alt="">
+      <h5 style="font-weight: 600;">Nike Air Max</h5>
+
+    </div>
+    <div class="col card-product">
+      <img src="/imgs/logo_jordan.svg" alt="">
+      <h5 style="font-weight: 600;">Nike Air Max</h5>
+
+    </div>
+    <div class="col card-product">
+      <img src="/imgs/logo_asics.svg" alt="">
+      <h5 style="font-weight: 600;">Nike Air Max</h5>
+
+    </div>
+  </div>
+</section>
 
 <?= $this->endSection() ?>
--- app/Views/layouts/BaseLayout.php
+++ app/Views/layouts/BaseLayout.php
@@ -5,47 +5,51 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>e-Commerce Shoes</title>
-  <!-- JQUERY -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-  <!-- TAILWIND CSS -->
-  <link href="<?= base_url() ?>css/output.css" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/218d5eb4ba.js" crossorigin="anonymous"></script>
+  <link href="<?= base_url() ?>css/custom.css" rel="stylesheet">
 </head>
 
 <body>
   <header class="pt-1 pb-2 text-center text-white bg-primary">Sign up and get 20% off to your first order. <a class="underline" href="/signup"> Sign Up Now</a></header>
-  <div class="flex items-center justify-between w-full h-10 gap-4">
-    <a class="tw-text-primary" href="#">IMAGINE</a>
-    <div class="flex gap-4">
-      <ul class="flex gap-4">
-        <li>
-          <a aria-current="page" href="#">Shop</a>
-        </li>
-        <li>
-          <a href="#">Top Selling</a>
-        </li>
-        <li>
-          <a>New Arrivals</a>
-        </li href="#">
-      </ul>
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <ul class="flex gap-4">
-        <li>
-          <a aria-current="page" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-        </li>
-        <li>
-          <a href="#"><i class="fa-regular fa-circle-user"></i></a>
-        </li>
-      </ul>
+
+  <nav class="navbar navbar-expand-lg" style="height: 100px;">
+    <div class="container">
+      <a class="navbar-brand" style="font-weight: 700; font-size: 32px" href="#">IMAGINE</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="gap-4 collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+        <ul class="mb-2 navbar-nav me-auto mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Top Selling</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-disabled="true">New Arrival</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary" type="submit">Search</button>
+        </form>
+        <div class="d-flex">
+          <a href="#" class="btn"><i class="fa-solid fa-cart-shopping" style="font-size: 20px"></i></a>
+          <a href="#" class="btn"><i class="fa-regular fa-circle-user" style="font-size: 30px;"></i></a>
+        </div>
+      </div>
     </div>
-  </div>
+  </nav>
 
   <main class="container">
     <article>
       <?= $this->renderSection('content') ?>
     </article>
   </main>
+
+  <!-- SCRIPT -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://kit.fontawesome.com/218d5eb4ba.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
