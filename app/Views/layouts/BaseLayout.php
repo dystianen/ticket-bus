8a3f23568c519ea6a782bca7a6a6d390e711f0da
Dystian En <distian.enyus@gmail.com>
2024-12-27 09:26:22 +0700

    fix: css

--- app/Views/layouts/BaseLayout.php
+++ app/Views/layouts/BaseLayout.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>e-Commerce Shoes</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <style lang="scss">
+  <style>
     :root {
       --bs-primary: #352f44;
       --bs-primary-rgb: 53, 47, 68;
