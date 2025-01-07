195d168cbda0a71e17a6168dd847f5e7c2829a70
Dystian En <distian.enyus@gmail.com>
2025-01-07 20:33:50 +0700

    change title icon

--- app/Views/layouts/base_layout.php
+++ app/Views/layouts/base_layout.php
@@ -7,6 +7,7 @@
   <title>IMAGINE!</title>
   <script src="https://unpkg.com/feather-icons"></script>
   <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+  <link rel="icon" href="<?= base_url() ?>assets/images/logo_imagine.png" type="image/x-icon" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <style>
     :root {
@@ -85,7 +86,9 @@
 
   <nav class="navbar navbar-expand-lg" style="height: 100px;">
     <div class="container">
-      <a class="navbar-brand text-primary" style="font-weight: 700; font-size: 32px; text-decoration: none" href="/">IMAGINE</a>
+      <a class="navbar-brand text-primary" style="font-weight: 700; font-size: 32px; text-decoration: none" href="/">
+        <img src="/assets/images/logo_imagine.png" alt="IMAGINE" width="60px" height="60px">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- app/Views/layouts/dashboard_layout.php
+++ app/Views/layouts/dashboard_layout.php
@@ -7,6 +7,7 @@
   <title>IMAGINE!</title>
   <script src="https://unpkg.com/feather-icons"></script>
   <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+  <link rel="icon" href="<?= base_url() ?>assets/images/logo_imagine.png" type="image/x-icon" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <style>
     :root {
